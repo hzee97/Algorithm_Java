6384480717a84f93c503f4fcd00bea81fd3bac5b
hzee97 <janghyeonji0830@naver.com>
2024-06-29 01:33:40 +0900

    코딩테스트준비_주요 함수 및 문제해결방법

---
+++ 코딩테스트_Java/주요문제해결.java
@@ -0,0 +1,26 @@
+// 문자열을 배열로 만들고 싶을 때
+String str = "Hello World!";
+String[] arr = str.split("");
+
+// 대소문자 변경
+String str = "Hello World!";
+str = str.toUpperCase();  // 대문자로 변경.
+str = str.toLowerCase();  // 소문자로 변경.
+
+// Math 라이브러리
+Math.max(10,2);   // 최댓값.
+Math.min(10,2);   // 최솟값.
+
+Math.abs();       // 절댓값.
+
+Math.ceil(10.3);  // 올림.
+Math.floor(10.3); // 내림.
+Math.round(10.3); // 반올림.
+
+Math.pow(2,2);    // 제곱.
+Math.sqrt(3);     // 제곱근.
+
+// format
+double a = 1.2345;
+String b = String.format("%.1f", a);
+
---
+++ 코딩테스트_Java/주요함수.java
@@ -0,0 +1,80 @@
+// 라이브러리
+import java.util.*;
+import java.io.*;
+
+// 변수 선언
+String[] arr = new String[5];
+int[] arr1 = {1,2,3};
+
+int n=3;
+int[] arr2 = new int[n];
+
+// array_sort
+int arr[] = {1,2,3,4,5};
+
+Arrays.sort(arr); // 오름차순
+Arrays.sort(arr, Collections.reverseOrder()); // 내림차순
+
+Arrays.sort(arr,0,3); // 부분정렬
+Arrays.binarySearch(arr,2); // 이진탐색 : binarySearch(탐색배열, 찾는 값)
+
+// 길이
+int[] arr = new int[3];
+arr.length;
+
+// String
+String str = "Hello World!";
+
+str.split(" ");
+str.substring(0,5);
+
+// HashMap
+HashMap<String, Integer> hash = new HashMap<>();
+
+hash.put("java",0); // key, value 넣기.
+hast.get("java");   // 키로 값 가져오기.
+
+hash.containsKey("java");  // "java" 가 hash 안에 있는지 판별.
+
+// ArrayList
+ArrayList<String> list = new ArrayList<>();  // 선언.
+
+list.add("java");         // "java" 삽입.
+list.add(0,"java");       // 0 번째 인덱스에 "java" 삽입.
+
+list.set(1,"python");     // 1 번째 인덱스의 값을 "python"으로 변경.
+
+list.remove(1);           // 1번째 인덱스의 값 삭제.
+
+list.contians("java");    // 값 "java"가 list 안에 있는지 존재유무 확인.
+
+list.indexOf("java");     // list 안에서 "java"를 찾아 그 인덱스를 리턴.
+
+// Iterator
+Iterator it = list.Iterator();
+
+it.hasNext()              // 오름차순 순회.
+it.hasPrevious()          // 내림차순 순회.
+
+// Queue
+Queue<Integer> q = new LinkedList<>();
+
+q.add(10);    // q 에 10 삽입.
+q.offer(7);   // q 에 7 삽입.
+
+q.remove();   // 삭제.
+q.poll();     // 삭제.
+
+q.clear();    // 초기화.
+
+q.isEmpty();  // q가 비어있는지 확인.
+
+// PriorityQueue
+PriorityQueue<Integer> pq = PriorityQueue<Integer>(); // 최소힙.
+PriorityQueue<Integer> pq = PriorityQueue<Integer>(Collections.reverseOrder()); // 최대힙.
+
+pq.add(3);    // 삽입.
+
+pq.remove();  // 삭제.
+
+pq.peek();    // root 값 추출.
