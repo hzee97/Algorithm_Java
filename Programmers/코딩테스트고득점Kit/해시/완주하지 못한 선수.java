153c2a67d68489b3070cd1488ead743850b14ff4
hzee97 <janghyeonji0830@naver.com>
2024-09-21 06:09:53 +0900

    고득점Kit.해시_완주하지 못한 선수

---
+++ Programmers/코딩테스트고득점Kit/해시/완주하지 못한 선수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        HashMap<String, Integer> hash = new HashMap<>();
+
+        for (String name : participant){
+            hash.put(name, hash.getOrDefault(name, 0)+1);
+        }
+        
+        for (String name : completion){
+            hash.put(name, hash.get(name)-1);
+        }
+        
+        for (String name : hash.keySet()) {
+            // 완주하지 못한 사람
+            if (hash.get(name) > 0) {
+                answer = name;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
