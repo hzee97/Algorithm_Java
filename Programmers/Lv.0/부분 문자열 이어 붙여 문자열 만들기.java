069a51aabeeb104d147aaa337cb44427677c1f3d
hzee97 <janghyeonji0830@naver.com>
2024-09-21 02:05:02 +0900

    Lv0.부분 문자열 이어 붙여 문자열 만들기

---
+++ Programmers/Lv.0/부분 문자열 이어 붙여 문자열 만들기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] my_strings, int[][] parts) {
+        String answer = "";
+        
+        for (int i=0; i<parts.length; i++){
+            for (int j=parts[i][0]; j<parts[i][1]+1; j++){
+                answer+=my_strings[i].charAt(j);
+            }
+        }
+        
+        return answer;
+    }
+}
