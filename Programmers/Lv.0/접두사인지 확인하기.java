1426e39089cc80033e440201e1cd4d11fdde3ede
hzee97 <janghyeonji0830@naver.com>
2024-09-21 01:42:33 +0900

    Lv0.접두사인지 확인하기

---
+++ Programmers/Lv.0/접두사인지 확인하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        int answer = 0;
+        boolean result = my_string.startsWith(is_prefix);
+        
+        if (result){
+            answer = 1;
+        } else {
+            answer = 0;
+        }
+        
+        return answer;
+    }
+}
